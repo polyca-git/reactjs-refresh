7b2fd41a3d3c9eeaa51f113d60709329e4bdc40a
polyca-git <egitim.foolvictim@gmail.com>
2021-06-05 22:44:49 +0200

    Creating a simple Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
-const App = () => {
-  return <h1 title='This works!'>A React App!</h1>; //React.createElement('h1',{title: 'This works!'},'A React App!');
-};
+//const App = () => {
+//  return <h1 title='This works!'>A React App!</h1>; //React.createElement('h1',{title: 'This works!'},'A React App!');
+//};
+
+class App extends React.Component{
+  render (){
+    return <h1 title='This works!'>A React App!</h1>;
+  }
+}
 
 export default App;
