34cb38ec759ac3ffb94842f3bcf6d5c71b0a2ece
polyca-git <egitim.foolvictim@gmail.com>
2021-06-05 22:41:40 +0200

    Another example of JSX translation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const App = () => {
-  return <h1>A React App!</h1>; //This is JSX, a html looking version of creating elements, normal verison is that->  React.createElement('h1',{},'A React App!');
+  return <h1 title='This works!'>A React App!</h1>; //React.createElement('h1',{title: 'This works!'},'A React App!');
 };
 
 export default App;
