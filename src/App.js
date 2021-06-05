a322f9a588061b8b2bf5b66398201679e510442b
polyca-git <egitim.foolvictim@gmail.com>
2021-06-05 22:38:46 +0200

    What is JSX

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const App = () => {
-  return <h1>A React App!</h1>;
+  return <h1>A React App!</h1>; //This is JSX, a html looking version of creating elements, normal verison is that->  React.createElement('h1',{},'A React App!');
 };
 
 export default App;
