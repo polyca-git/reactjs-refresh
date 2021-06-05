c547828abc3f898ab07b87085bfdd58c2ae0997c
polyca-git <egitim.foolvictim@gmail.com>
2021-06-05 22:47:10 +0200

    A little info about returing JMX in React

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 //const App = () => {
-//  return <h1 title='This works!'>A React App!</h1>; //React.createElement('h1',{title: 'This works!'},'A React App!');
+//  return <h1 title='This works!'>A React App!</h1>; //A react component has to return JSX. If we would try to return an objet we would get an error.
 //};
 
 class App extends React.Component{
   render (){
-    return <h1 title='This works!'>A React App!</h1>;
+    return <h1 title='This works!'>A React App!</h1>; //A react component has to return JSX. If we would try to return an objet we would get an error.
   }
 }
 
