c56505afaa4d0b0c08fe17ecc98b5f99a5098953
polyca-git <egitim.foolvictim@gmail.com>
2021-06-16 21:24:57 +0200

    States

--- src/App.js
+++ src/App.js
@@ -4,28 +4,30 @@ import './App.css'; //We imported our CSS file
 import GoalList from './components/GoalList/GoalList'; //Example import of a component we have created.
 import NewGoal from './components/NewGoal/NewGoal';
 
-const App =()=> {
-  
-    const [courseGoals, setCourseGoals] = useState([
-      { id: 'cg1', text: 'Finish the course.' },
-      { id: 'cg2', text: 'Learn the main topic.' },
-      { id: 'cg3', text: 'Help other students.' }
-    ]);//useState 2 parametre tasir , ilki veriyi, 2. si veriyi update etme fonksiyonunu
-    //state in amaci canli veri degisimi, bu veri degistikce tekrar render edilecek
+const App = () => {
+
+  const [courseGoals, setCourseGoals] = useState([
+    { id: 'cg1', text: 'Finish the course.' },
+    { id: 'cg2', text: 'Learn the main topic.' },
+    { id: 'cg3', text: 'Help other students.' }
+  ]);//useState 2 parametre tasir , ilki veriyi, 2. si veriyi update etme fonksiyonunu
+  //state in amaci canli veri degisimi, bu veri degistikce tekrar render edilecek
 
 
-    const addNewGoalHandler = newGoal => {
-      setCourseGoals(courseGoals.concat(newGoal));//4.gelen newGoal degeriyle courseGoals state i update ediliyor 
-      //ve canli olarak listeye yeni eleman ekleniyor
-      
-    };
-    console.log(courseGoals);
-    return (<div className="course-goals">
-      <h2>Course Goals</h2>
-      <NewGoal onAddGoal={addNewGoalHandler} />
-      <GoalList goals={courseGoals} />
-    </div>);//1.NewGoal componentine onAddGoal diye bir prop yolluyoruz, o da addNewGoalHandler fonksiyonunu tasiyor
-  
+
+
+  const addNewGoalHandler = (newGoal) => {
+    //setCourseGoals(courseGoals.concat(newGoal));
+    setCourseGoals(prevCourseGoals => prevCourseGoals.concat(newGoal));
+
+  };
+
+  return (<div className="course-goals">
+    <h2>Course Goals</h2>
+    <NewGoal onAddGoal={addNewGoalHandler} />
+    <GoalList goals={courseGoals} />
+  </div>);//1.NewGoal componentine onAddGoal diye bir prop yolluyoruz, o da addNewGoalHandler fonksiyonunu tasiyor
+
 }
 
 export default App;
--- src/components/NewGoal/NewGoal.js
+++ src/components/NewGoal/NewGoal.js
@@ -1,24 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './NewGoal.css';
 
 
 const NewGoal = props => {
+    const [enteredText, setEnteredText]= useState('');
     const addGoalHandler = event => { //the parameter named event is known by react like props parameter, 
         //this parameter carries information of the event that triggered.
         event.preventDefault();
-        
-        const newGoal = [{
+
+        const newGoal = {
             id: Math.random().toString(),
-            text: 'My new goal!'
-        }];
+            text: enteredText
+        };//fixed the issue from the old commit by removing []'s
+
+        setEnteredText(''); 
 
         props.onAddGoal(newGoal); //3. proplardan onAddGoal, newGoal degiskenini tasiyor, 
         //bu da dolayli olarak App.js teki addNewGoalHandler degiskenini tetikliyor
     };
 
-    
+    const textChangeHandler = event => {
+        setEnteredText(event.target.value);
+    };
+
+
     return (<form className='new-goal' onSubmit={addGoalHandler}>
-        <input type='text' />
+        <input type='text' value={enteredText} onChange={textChangeHandler} />
         <button type='submit'>Add Goal</button>
     </form>); //in react we have special properties like onSubmit or many others  for every JSX component. 
     //in here we used onSubmit and triggered the function named addGoalHandler
